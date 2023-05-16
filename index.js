561f366610c6f9d120b6aefb8d62365f4c00d02e
Penguuu <pengvang95@yahoo.com>
2023-05-15 22:09:31 -0500

    adding

---
+++ index.js
@@ -0,0 +1,63 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Triangle, Circle, Square } = require("./shapes");
+
+function generateLogo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "text",
+        message: "Enter up to three characters for the logo:",
+        validate: function (input) {
+          return input.length <= 3 ? true : "Enter up to three characters.";
+        },
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message: "Enter the text color (keyword or hexadecimal number):",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "Choose a shape:",
+        choices: ["Triangle", "Circle", "Square"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter the shape color (keyword or hexadecimal number):",
+      },
+    ])
+    .then((answers) => {
+      const { text, textColor, shape, shapeColor } = answers;
+
+      let selectedShape;
+      switch (shape) {
+        case "Triangle":
+          selectedShape = new Triangle();
+          break;
+        case "Circle":
+          selectedShape = new Circle();
+          break;
+        case "Square":
+          selectedShape = new Square();
+          break;
+      }
+
+      selectedShape.setColor(shapeColor);
+
+      const svgString = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${selectedShape.render()}
+        <text x="150" y="150" fill="${textColor}" text-anchor="middle" font-size="40">${text}</text>
+      </svg>`;
+
+      fs.writeFile("logo.svg", svgString, (err) => {
+        if (err) throw err;
+        console.log("Generated logo.svg");
+      });
+    });
+}
+
+generateLogo();
